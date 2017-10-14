924535d93532803a59e84fafbe88fef82a9da09d
Nitin thakkar <nitinthakkar@Nitins-MacBook-Air.local>
2017-10-14 14:40:51 +0530

    addtodo update

--- src/component/index.js
+++ src/component/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { View, Text}  from 'react-native';
 import AddTodo from '../container/AddTodo';
 export default class Root extends React.Component {
+
   render() {
+
     return (
-      <View>
-       <AddTodo />
+  <View>
+      <AddTodo/>
       <Text> Hello Dalip</Text>
       </View>
     )
--- src/container/AddTodo.js
+++ src/container/AddTodo.js
@@ -1,46 +1,17 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { connect } from 'react-redux';
-import { TextInput ,View,TouchableHighlight,Text} from 'react-native';
-import * as TodoActionCreators from '../action';
- export default class AddTodo extends React.Component {
-  return
-  (
-    <View style={
-      {
-        flex:1,
-      }
-    }>
-        <Text>Add Task!!</Text>
-        <TextInput style={
-          {
-            height:100,
-            width:300
-          }
-        } onChangeText={(typeText)=>{
-          console.log("text:",typeText);
-          input=typeText;
-          console.log("input:",input);
-        }} placeholder="write here"/>
-        <TouchableHighlight onPress={()=>{
-          console.log("text:",input);
+import { TextInput,View,TouchableHighlight,Text} from 'react-native';
+ class AddTodo extends React.Component {
+   render() {
+     return (
+       <View>
+       <Text>Bingo</Text>
+       </View>
+     )
 
-        }} style={
-          {
-            backgroundColor:'#eeffcd',
-            height:50,
-            width:200
-          }
-        }>
-        <Text style={
-          {
-            alignSelf:'center',
-            fontSize:25
-          }
-        }>Submit</Text>
-        </TouchableHighlight>
-    </View>
-  )
+   }
+
 }
-//AddTodo = connect()(AddTodo)
-
-//export default AddTodo
+AddTodo = connect()(AddTodo)
+ 
+export default AddTodo
